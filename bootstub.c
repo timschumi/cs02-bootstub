7d1a880839028ceb8f8d12719164eae82397669a
Tim Schumacher <timschumi@gmx.de>
2022-08-27 21:39:46 +0200

    Greet nicely

--- bootstub.c
+++ bootstub.c
@@ -1,7 +1,7 @@
 void (*_log_message)(char*, ...) = (void*)0xae203534;
 
-__attribute__((naked)) void _start() {
-    _log_message((char*)0xae25ec74, "blahblah");
+void _start() {
+    _log_message("Hello from bootstub!");
 
     for (;;)
         asm volatile("wfi");
