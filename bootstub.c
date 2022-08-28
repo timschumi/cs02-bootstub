796106f8837e5145982323bcb02e92d0b3dea4e6
Tim Schumacher <timschumi@gmx.de>
2022-08-28 02:02:10 +0200

    Test force-enabling USB

--- bootstub.c
+++ bootstub.c
@@ -10,6 +10,7 @@ void _start() {
 }
 
 void (*_log_message)(char*, ...) = (void*)0xae203534;
+void (*_uart_print_character)(int) = (void*)0xae2026e8;
 uint8_t (*__microusb_register_read)(uint16_t reg, uint8_t* value) = (void*)0xae230200;
 uint8_t (*__microusb_register_write)(uint16_t reg, uint8_t value) = (void*)0xae23027c;
 
@@ -45,9 +46,41 @@ void microusb_dump_registers() {
     _log_message("\n");
 }
 
+int last_buffer_offset = 0;
+
+void microusb_force_usb() {
+    uint8_t value = 0;
+    __microusb_register_read(0x2, &value);
+    __microusb_register_write(0x2, value & 0b11111011);
+    __microusb_register_write(0x13, 0x24);
+
+    last_buffer_offset = (*(int*)0xae8ffffc) & 0x6ffff;
+}
+
+void microusb_unforce_usb() {
+    uint8_t value = 0;
+    __microusb_register_read(0x2, &value);
+    __microusb_register_write(0x2, value | 0b00000100);
+    __microusb_register_write(0x13, 0x00);
+
+    int current_buffer_offset = (*(int*)0xae8ffffc) & 0x6ffff;
+
+    while (last_buffer_offset != current_buffer_offset) {
+        _uart_print_character(*((char*)0xae900000 + last_buffer_offset));
+        last_buffer_offset = (last_buffer_offset + 1) & 0x6ffff;
+    }
+}
+
 void main() {
     _log_message("Hello from bootstub!\n");
 
     _log_message("Here is an USB register dump:\n");
     microusb_dump_registers();
+
+    microusb_force_usb();
+
+    _log_message("USB registers while force-enabling USB:\n");
+    microusb_dump_registers();
+
+    microusb_unforce_usb();
 }
